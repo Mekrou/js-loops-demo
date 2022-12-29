1942b27946a7fa9bc857c781ff4284a4a21522d2
mekrm <mills.ryan.011@gmail.com>
2022-12-29 19:56:08 +0000

    basic while loop

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-for (let i = 1; i <= 10; i++)
-{
-    console.log(`i is: ${i}`);
-    for(let j = 1; j < 4; j++) {
-        console.log(`   j is: ${j}`);
-    }
+// for (let i = 1; i <= 10; i++)
+// {
+//     console.log(`i is: ${i}`);
+//     for(let j = 1; j < 4; j++) {
+//         console.log(`   j is: ${j}`);
+//     }
+// }
+
+let count = 0;
+while (count < 10) {
+    count++;
+    
+    console.log(count);
+    
 }
