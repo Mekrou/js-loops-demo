8cd2d33560ea6ef5f998c2f0f3cef6809056c030
mekrm <mills.ryan.011@gmail.com>
2022-12-29 19:51:51 +0000

    basic nested loop demonstration

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
+for (let i = 1; i <= 10; i++)
+{
+    console.log(`i is: ${i}`);
+    for(let j = 1; j < 4; j++) {
+        console.log(`   j is: ${j}`);
+    }
 }
