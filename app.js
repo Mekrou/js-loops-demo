83f85b1e02cee0ab4f7dd185efca0f482d8e93a8
mekrm <mills.ryan.011@gmail.com>
2022-12-30 18:42:29 +0000

    for of loops... <3

--- app.js
+++ app.js
@@ -16,23 +16,47 @@
 
 // Guessing Game:
 
-const answer = Math.floor(Math.random() * 100) + 1;
-console.log(answer);
+// const answer = Math.floor(Math.random() * 100) + 1;
+// console.log(answer);
 
-let input = parseInt(prompt("Guess my number between 1 and 100"));
-let numGuesses = 1;
+// let input = parseInt(prompt("Guess my number between 1 and 100"));
+// let numGuesses = 1;
 
-while (input !== answer)
+// while (input !== answer)
+// {
+//     if (input > answer)
+//     {
+//         input = prompt("Your guess was too high. Try again: ");
+//     }
+//     else if (input < answer)
+//     {
+//         input = prompt("Your guess was too low. Try again: ");
+//     } 
+//     numGuesses++;
+// }
+
+// console.log(`Correct! You guessed ${numGuesses} times`);
+
+// ==============================================
+// For of loop
+
+const subreddits = ['cringe', 'books', 'coffee', 'mushrooms', 'funny', 'memes', 'dunkin'];
+
+for (let subreddit of subreddits)
 {
-    if (input > answer)
-    {
-        input = prompt("Your guess was too high. Try again: ");
-    }
-    else if (input < answer)
-    {
-        input = prompt("Your guess was too low. Try again: ");
-    } 
-    numGuesses++;
+    console.log(subreddit);
 }
 
-console.log(`Correct! You guessed ${numGuesses} times`);
+// even more powerful when it comes to multi dimensional arrays
+
+const seatingChart = [
+    ['Ryan', 'Caitlyn', 'Simon'],
+    ['Daxton','Jet','Kenny'],
+    ['Skylar','Evan','Delaney']
+]
+
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
+}
